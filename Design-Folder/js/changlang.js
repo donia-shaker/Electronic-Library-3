de58da5098a3fa801f9cfbd3c80b53b1881acdfd
Donia Shaker <donia.shaker11@gmail.com>
2022-01-26 16:43:47 +0300

    Add Multi-Language

---
+++ Design-Folder/js/changlang.js
@@ -0,0 +1,12 @@
+function changeLanguage() {
+  var checkBox = document.getElementById("lang");
+  var lable = document.getElementById("changLan");
+  if (checkBox.checked) {
+    document.documentElement.style.setProperty("--text-align", "left");
+    document.documentElement.style.setProperty("--direction", "ftl");
+    lable.innerHTML = "العربية ";
+  } else {
+    document.documentElement.style.setProperty("--text-align", "right");
+    document.documentElement.style.setProperty("--direction", "rtl");
+  }
+}
